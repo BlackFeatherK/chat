e3dca22ff2981d326cbddb372dee509fd99eaf89
BlackFeather <she3354248@yahoo.com.tw>
2017-03-20 17:27:58 +0800

    Use server-side partial

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -7,8 +7,9 @@ class MessagesController < ApplicationController
     @message.user = current_user
 
     if @message.save
-      data = { content: @message.content, created_at: @message.created_at.to_s, user_name: @message.user_name }
-      ActionCable.server.broadcast("room_#{@room.id}", data)
+      # data = { content: @message.content, created_at: @message.created_at.to_s, user_name: @message.user_name }
+      html = ApplicationController.renderer.render(partial: 'messages/message', locals: {message: @message})
+      ActionCable.server.broadcast("room_#{@room.id}", html)
       render js: '$("#message_content").val("");'
     else
       render js: 'alert("No content");'
